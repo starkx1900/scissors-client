ed52edf836a4cbfad0d43e4616e739b483aa6e84
starkx1900 <Alfredstark01@gmail.com>
2024-08-27 23:04:57 +0100

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -126,7 +126,7 @@ const LoginPage = () => {
         </Form>
 
         <div className="mt-4 text-center text-sm">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/auth/signup" className="underline">
             Sign up
           </Link>
