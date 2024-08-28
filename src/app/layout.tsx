59478f212d81ec9eca29687f504e170bd15f50da
starkx1900 <Alfredstark01@gmail.com>
2024-08-28 12:02:28 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <Suspense>
             <Navbar />
-            <main className="w-full max-w-5xl">{children}</main>
+            <main className="w-full max-w-5xl mx-auto">{children}</main>
             <Toaster />
           </Suspense>
         </body>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,31 +2,33 @@
 
 import { useAuth } from '@/context/AuthContext';
 import { LogOut, Scissors } from 'lucide-react';
+import Link from 'next/link';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
 
   return (
-    <nav className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <div className="flex w-full items-center justify-between border-b border-gray-300 bg-gradient-to-b from-zinc-200 px-2 sm:px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border ">
+    <nav className="z-10 w-full max-w-5xl mx-auto items-center justify-between font-mono text-sm lg:flex border-b border-gray-300 bg-gradient-to-b from-zinc-200 px-2 sm:px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border ">
+      <Link href="/">
         <span className="flex uppercase font-bold gap-1">
           <Scissors size={20} className="text-blue-600" />
           SCISSORS
         </span>
+      </Link>
 
+      <div className="flex font-bold gap-1">
+        <span>Welcome</span>
         {user && (
-          <div className="flex font-bold gap-1">
-            <span>Welcome,</span>
+          <>
             {/* <span className="flex gap-2"> */}
-            {/* <User size={20} className="text-blue-600" /> */}
-            {user.name}
+            {/* <User size={20} className="text-blue-600" /> */}, {user.name}
             {/* </span> */}
             <LogOut
               onClick={logout}
               size={20}
               className="ml-4 text-red-600 cursor-pointer"
             />
-          </div>
+          </>
         )}
       </div>
     </nav>
