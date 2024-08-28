ea838b5232a95c902340cd609f7ec3450241b681
starkx1900 <Alfredstark01@gmail.com>
2024-08-28 12:06:08 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const { user, logout } = useAuth();
 
   return (
-    <nav className="z-10 w-full max-w-5xl mx-auto items-center justify-between font-mono text-sm lg:flex border-b border-gray-300 bg-gradient-to-b from-zinc-200 px-2 sm:px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border ">
+    <nav className="z-10 w-full max-w-5xl mx-auto flex items-center justify-between font-mono text-sm lg:flex border-b border-gray-300 bg-gradient-to-b from-zinc-200 px-2 sm:px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border ">
       <Link href="/">
         <span className="flex uppercase font-bold gap-1">
           <Scissors size={20} className="text-blue-600" />
